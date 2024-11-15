97271f72df37bf864544e8d8a4a34796c1174dc4
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2024-11-15 16:43:10 +0100

    Make span's pointer and size private members

--- src/span.hpp
+++ src/span.hpp
@@ -36,34 +36,35 @@ class span {
 
     static constexpr size_type extent = Extent;
 
-    pointer ptr;
-
     template <std::size_t N>
     constexpr span(type_identity_t<element_type> (&arr)[N]) noexcept
-        : ptr(static_cast<pointer>(arr)) {}
+        : m_ptr(static_cast<pointer>(arr)) {}
 
     template <typename U, std::size_t N,
               std::enable_if_t<is_compatible_array<U, N>::value, bool> = true>
     constexpr span(std::array<U, N> &arr) noexcept
-        : ptr(static_cast<pointer>(arr.data())) {}
+        : m_ptr(static_cast<pointer>(arr.data())) {}
 
     template <typename U, std::size_t N,
               std::enable_if_t<is_compatible_array<U, N>::value, bool> = true>
     constexpr span(const std::array<U, N> &arr) noexcept
-        : ptr(static_cast<pointer>(arr.data())) {}
+        : m_ptr(static_cast<pointer>(arr.data())) {}
 
-    constexpr span(const span<T> &other) noexcept : ptr(other.ptr) {}
+    constexpr span(const span<T> &other) noexcept : m_ptr(other.m_ptr) {}
 
     constexpr std::size_t size() const noexcept { return extent; }
     constexpr T &operator[](std::size_t idx) const {
         if (idx >= extent) {
             throw std::out_of_range("span index out of range");
         }
-        return this->ptr[idx];
+        return this->m_ptr[idx];
     }
 
-    constexpr reference front() const noexcept { return ptr[0]; }
-    constexpr reference back() const noexcept { return ptr[extent - 1]; }
+    constexpr reference front() const noexcept { return m_ptr[0]; }
+    constexpr reference back() const noexcept { return m_ptr[extent - 1]; }
+
+  private:
+    pointer m_ptr;
 };
 
 template <typename T>
@@ -87,34 +88,39 @@ class span<T, dynamic_extent> {
 
     static constexpr size_type extent = dynamic_extent;
 
-    T *ptr;
-    std::size_t len;
 
-    constexpr span() noexcept : ptr(nullptr), len(0) {}
-    constexpr span(pointer ptr, size_type size) : ptr(ptr), len(size) {}
+    constexpr span() noexcept : m_ptr(nullptr), m_len(0) {}
+    constexpr span(pointer ptr, size_type size) : m_ptr(ptr), m_len(size) {}
 
     template <typename U, std::size_t N>
     constexpr span(type_identity_t<element_type> (&arr)[N]) noexcept
-        : ptr(arr), len(N) {}
+        : m_ptr(arr), m_len(N) {}
 
     template <typename U, std::size_t N,
               std::enable_if_t<is_compatible_array<U>::value, bool> = true>
-    constexpr span(std::array<U, N> arr) noexcept : ptr(arr), len(N) {}
+    constexpr span(std::array<U, N> arr) noexcept : m_ptr(arr), m_len(N) {}
 
     constexpr span(const span<T> &other) noexcept
-        : ptr(other.ptr), len(other.len) {}
+        : m_ptr(other.m_ptr), m_len(other.len) {}
 
     constexpr std::size_t size() const noexcept { return this->len; }
 
-    constexpr T &operator[](std::size_t idx) const {
+    constexpr reference operator[](std::size_t idx) const {
         if (idx >= this->len) {
             throw std::out_of_range("span index out of range");
         }
-        return this->ptr[idx];
+        return this->m_ptr[idx];
     }
 
-    constexpr reference front() const noexcept { return ptr[0]; }
-    constexpr reference back() const noexcept { return ptr[this->len - 1]; }
+    constexpr bool empty() const noexcept { return size() == 0; }
+
+    constexpr reference front() const noexcept { return m_ptr[0]; }
+    constexpr reference back() const noexcept { return data()[size() - 1]; }
+    constexpr pointer data() const noexcept { return m_ptr; }
+
+  private:
+    pointer m_ptr;
+    std::size_t m_len;
 };
 
 template <typename T, std::size_t N>
